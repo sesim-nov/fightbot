3ed71120de32f11e88feb863d6555aee5164a38d
Sesim <1330844+sesim-nov@users.noreply.github.com>
2025-07-29 21:54:28 -0400

    loaded poise boilerplate

---
+++ src/commands.rs
@@ -0,0 +1,14 @@
+use poise::serenity_prelude::{self as serenity};
+use crate::{Context, Error};
+
+
+#[poise::command(slash_command, prefix_command)]
+pub async fn age(
+    ctx: Context<'_>,
+    #[description = "Selected user"] user: Option<serenity::User>,
+) -> Result<(), Error> {
+    let u = user.as_ref().unwrap_or_else(|| ctx.author());
+    let response = format!("{}'s account was created at {}", u.name, u.created_at());
+    ctx.say(response).await?;
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use std::sync::{Arc, Mutex};
+
+use poise::serenity_prelude as serenity;
+use dotenv::dotenv;
+
+mod commands;
+
+struct Data {
+    counter: Arc<Mutex<i32>>,
+} // User data, which is stored and accessible in all command invocations
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let token = std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN");
+    let intents = serenity::GatewayIntents::non_privileged();
+    //intents.insert(serenity::GatewayIntents::MESSAGE_CONTENT); <- uncomment to add message content parsing for slash commands. 
+
+    let prefix_options = poise::PrefixFrameworkOptions{
+        prefix: Some("+".to_string()),
+        ..Default::default()
+    };
+
+    let options = poise::FrameworkOptions{
+        commands: vec![commands::age()],
+        pre_command: |ctx| {
+            Box::pin(async move {
+                println!("Executing command: {}", ctx.command().qualified_name)
+            })
+        },
+        prefix_options: prefix_options,
+        ..Default::default()
+    };
+
+    let framework = poise::Framework::builder()
+        .options(options)
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {
+                    counter: Arc::new(Mutex::new(0)),
+                })
+            })
+        })
+        .build();
+
+    let client = serenity::ClientBuilder::new(token, intents)
+        .framework(framework)
+        .await;
+    client.unwrap().start().await.unwrap();
+}
